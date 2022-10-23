f3f643d636bea5f126f5e7b27ee7d6f6b090642b
Roger-Franco <rogerfrancogalt@gmail.com>
2022-10-23 19:03:57 -0400

    Pequena ajustada no código

--- src/Api.js
+++ src/Api.js
@@ -1,20 +1,24 @@
 // import firebase from "firebase/app";
 // import "firebase/firebase-auth";
 // import "firebase/firebase-firestore";
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
+// import firebase from "firebase/compat/app";
+// import "firebase/compat/auth";
+// import "firebase/compat/firestore";
+import { initializeApp, auth } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 import firebaseConfig from "./firebaseConfig";
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 // const db = getFirestore(firebaseApp);
-const db = firebaseApp.firestore();
+const db = getFirestore();
 
 export default {
   googleLogar: async () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    let result = await firebase.auth().signInWithPopup(provider);
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    let result = await signInWithPopup(auth, provider);
     return result;
   },
   // emailPasswordLogar: async ({ email, senha }) => {
